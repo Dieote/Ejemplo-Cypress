258cbb8e2317e53650abbab1ed2729de064d0eda
Dieote <gomezmdiego97@gmail.com>
2023-02-25 13:53:54 +0100

    Cambio1
    Organizar carpetas en repositorio

--- pruebas.cy.js
+++ pruebas.cy.js
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Pruebas Automaticas Cypress', () => {
+  beforeEach(() => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/')
+ })
+
+it("Elemento basicos pagina inicio e ingreso", () => {
+  //cy.visit("https://opensource-demo.orangehrmlive.com/web/")
+
+  cy.get('.orangehrm-login-branding').should("be.visible")
+  cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').should("be.visible")
+  cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').should("be.visible")
+  cy.get('.oxd-button').should("be.visible")
+  cy.get('.orangehrm-copyright-wrapper > :nth-child(1)').should("be.visible")
+  cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').click()
+  cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Admin")
+  cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').click()
+  cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123")
+  cy.get('.oxd-button').click()
 })
+
+ })
