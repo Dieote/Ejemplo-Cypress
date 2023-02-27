2e8812731a60730bcd691e2ada59668603d30fb5
Dieote <gomezmdiego97@gmail.com>
2023-02-27 10:35:24 +0100

    Usuarios
    Eliminar usuario

--- pruebas.cy.js
+++ pruebas.cy.js
@@ -16,6 +16,43 @@ it("Elemento basicos pagina inicio e ingreso", () => {
   cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').click()
   cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123")
   cy.get('.oxd-button').click()
+  cy.get('.oxd-brand-banner > img').should("be.visible")
+
 })
 
+it("Navegacion de barra lateral", () => {
+
+  cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').click()
+  cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Admin")
+  cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').click()
+  cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123")
+  cy.get('.oxd-button').click()
+  cy.get(':nth-child(1) > .oxd-main-menu-item').should("be.visible")
+  cy.get(':nth-child(2) > .oxd-main-menu-item').should("be.visible")
+  cy.get(':nth-child(3) > .oxd-main-menu-item').should("be.visible")
+  cy.get(':nth-child(4) > .oxd-main-menu-item').should("be.visible")
+  cy.get(':nth-child(5) > .oxd-main-menu-item').should("be.visible")
+  cy.get(':nth-child(6) > .oxd-main-menu-item').should("be.visible")
+  cy.get(':nth-child(7) > .oxd-main-menu-item').should("be.visible")
+  cy.get(':nth-child(8) > .oxd-main-menu-item').should("be.visible")
+  cy.get(':nth-child(9) > .oxd-main-menu-item').should("be.visible")
+  cy.get(':nth-child(10) > .oxd-main-menu-item').should("be.visible")
+  cy.get(':nth-child(11) > .oxd-main-menu-item').should("be.visible")
+
+})
+it("Eliminar un Usuario", () => {
+
+  cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').click()
+  cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Admin")
+  cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').click()
+  cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123")
+  cy.get('.oxd-button').click()
+  cy.get(':nth-child(1) > .oxd-main-menu-item').click()
+  cy.get('.oxd-layout-context').scrollTo("bottom" ,{ensureScrollable: false})
+  cy.get(':nth-child(48) > .oxd-table-row > :nth-child(6) > .oxd-table-cell-actions > :nth-child(1)').click()
+  cy.get('.orangehrm-text-center-align > .oxd-text').should("be.visible")
+  cy.get('.orangehrm-text-center-align > .oxd-text').contains('The selected record will be permanently deleted. Are you sure you want to continue?')
+  cy.get('.oxd-button--label-danger').click()
+
+})
  })
