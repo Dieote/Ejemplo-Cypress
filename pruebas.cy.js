830481d500a45a8e5e2a5eb10d8820c8970e76a9
Dieote <gomezmdiego97@gmail.com>
2023-04-06 11:32:51 +0200

    Select Delete

--- pruebas.cy.js
+++ pruebas.cy.js
@@ -56,31 +56,32 @@ describe("Pruebas Automaticas Cypress", () => {
       .contains('The selected record will be permanently deleted. Are you sure you want to continue?')
       cy.get('.oxd-button--label-danger').click()
   
-    })*/
+    })
 
-  it("Elegir un desplegable de la lista", () => {
-    cy.get(
-      ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input"
-    ).click();
-    cy.get(
-      ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input"
-    ).type("Admin");
-    cy.get(
-      ":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input"
-    ).click();
-    cy.get(
-      ":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input"
-    ).type("admin123");
-    cy.get(".oxd-button").click();
+*/
+  it.only("Prueba de validacion - Eliminar usuario", () => {
 
-        //check text in select plus time
-    cy.get(":nth-child(5) > .oxd-main-menu-item",{ timeout: 10000 }).click();
-    cy.get(':nth-child(3) > .oxd-grid-4')
-      .find('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text')
-      .should('have.text','-- Select --')
 
-      //choose option "manual"
-    cy.get('.oxd-select-text').should('have.text',"Manual")
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Admin")
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123")
+    cy.get('.oxd-button').click()
+    cy.get(':nth-child(1) > .oxd-main-menu-item').click()
+    cy.get('.orangehrm-header-container > .oxd-button').click()
+    cy.get(':nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text').click()
+    cy.get('.oxd-select-dropdown > :nth-child(2)').click()
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text').click()
+    cy.get('.oxd-select-dropdown > :nth-child(2)').click()
+    cy.get('.oxd-autocomplete-text-input > input').type("Charlie Carter")                 
+    // Si da error aca es porque alguien borro a ese empleado
+    cy.wait(4000);
+    cy.get('.oxd-autocomplete-dropdown').click()
+    cy.get(':nth-child(4) > .oxd-input-group > :nth-child(2) > .oxd-input').type("UsuarioPrueba123")
+    cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').type("Pass1234!")
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Pass1234!").type('{enter}')
+    cy.get('.oxd-button--secondary').click()
+    cy.wait(4000);
+    cy.get('.oxd-button--secondary').click()
+    cy.get('.oxd-text--toast-title').contains("Success")
 
-  });
+});
 });
